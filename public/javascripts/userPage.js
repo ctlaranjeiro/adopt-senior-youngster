0bab85aede8ff0cf3a15ad4049880584b651ad60
Bruno <bpais@outlook.pt>
2020-05-09 06:52:08 +0100

    working on user page issues-v2 with css

--- public/javascripts/userPage.js
+++ public/javascripts/userPage.js
@@ -1,18 +1,26 @@
-const User = require('../models/user');
-const Volunteer = require('../models/volunteer');
+// document.addEventListener('DOMContentLoaded', () => {
+  
+//   console.log('user id', $('body').html(templateCompile(data)));
 
-if (!user.assignedVolunteers.length) {
-  document.getElementById('user-volunteers').innerHTML = 
-  `
-    <p>{{user.firstName}} {{user.lastName}} has no volunteers assigned!</p>
-  `;
-} else {
-  let assignedVolunteers = user.assignedVolunteers;
-  document.getElementById('volunteersList').innerHTML = assignedVolunteers.forEach(volunteer => {
-    `
-      <li>
-        
-      </li>
-    `
-  });
-}
+// }, false);
+// if (user.assignedVolunteers) {
+//   document.getElementById('userVolunteers').innerHTML = '';
+//   document.getElementById('userVolunteers').innerHTML = 
+//   `
+//     <p>${user.firstName} ${user.lastName} has no volunteers assigned!</p>
+//   `;
+//   console.log('no volunteers');
+// } else {
+//   document.getElementById('userVolunteers').innerHTML = '';
+//   let assignedVolunteers = user.assignedVolunteers;
+//   document.getElementById('volunteersList').innerHTML =
+//   assignedVolunteers.forEach(volunteer => 
+//     `
+//       <li>
+//         <span>${volunteer.firstName}</span>
+//         <div class="avatar"></div>
+//       </li>
+//     `
+//   );
+//   console.log('with volunteers');
+// }
