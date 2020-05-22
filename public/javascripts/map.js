68d4f35d3ad411c4ad6704503af6003ffde92ac7
Bruno <bpais@outlook.pt>
2020-05-22 23:31:03 +0100

    map is rendered but has no markers

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,67 +1,40 @@
+// EXAMPLE FOR ADDING A MARKER:
+// addMarker({
+//   coords: {lat: 38.7166700, lng: -9.1333300},
+//   content: '<p>Lisbon</p>'
+// });
 
-function address() {
-  let subjects = document.querySelectorAll('div._id');
-  console.log(subjects);
-  
-  let moradas = [];
-  for(let j = 0; j < subjects.length; j ++) {
-    
-    let id = subjects[j].innerHTML;
-    console.log('ID:', id);
-    let morada = document.getElementById(id+'morada').innerHTML;
-    console.log('Morada:', morada);
-    moradas.push(morada);
-    // geocode();
-    // initMap();
-    
-  }
-   console.log(moradas);
-  // let adress;
-  for (let i = 0; i < moradas.length; i ++) {
-    let address = moradas[i];
-    return address;
-  }
-}
+// function addMarker(props) {
+//   let marker = new google.maps.Marker ({
+//     position: props.coords,
+//     map: map
+//   });
 
-function geocode() {
-  axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-    params: {
-      adress: address(),
-      // key: app.locals.googleMapsKey //NÃO ESTÁ A ASSUMIR
-      key: 'AIzaSyDmvhJhg_f-9Tozc1F_XkMOcaHfGCy14Nc'
-    }
-  })
-  .then((res) => {
-    console.log(res);
-    let formattedAddress = res.data.results[0].formatted_address;
-    let latitude = res.data.results[0].reometry.morada.lat;
-    let longitude = res.data.results[0].reometry.morada.lng;
+//   // Shows an infowindow if the parameter is passed( could be the name of the person)
+//   if(props.content) {
+//     let infoWindow = mew google.maps.InfoWindow({
+//       content: props.content
+//     });
 
-    console.log(formattedAddress);
-
-    console.log('lat:', latitude, 'lng:', longitude);
-
-    return {lat: latitude, lng: longitude};
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
-}
-
-// geocode();
+//     marker.addListener('click',function(){
+//       infoWindow.open(map, marker);
+//     });
+//   }
+// }
 
 function initMap() {
 
-  // default map options
+  // DEFAULT MAP OPTIONS:
   let options = {
     zoom: 12,
     center: {lat: 38.7166700, lng: -9.1333300}
   };
 
-  // set the map
+  // SET THE MAP:
   let map = new google.maps.Map(document.getElementById('map'), options);
 
-  // identifier marker
+  // SET IDENTIFYER MARKER:
+  // addMarker();   -----> chama o marker que recebe as coords da geocode()
   let marker = new google.maps.Marker({
     position: geocode(),
     // position: {lat: 38.7166700, lng: -9.1333300},
@@ -71,8 +44,47 @@ function initMap() {
 
 initMap();
 
-// subjects.forEach(subject => {
-//   let id = subject.innerHTML;
-//   console.log('ID:', id);
-//   initMap();
-// });
+
+// TO GET THE COORDENATES THROUGH THE ADDRESS:
+function geocode() { 
+let subjects = document.querySelectorAll('div._id');
+
+for (let j = 0; j < subjects.length; j ++) {
+  let id = subjects[j].innerHTML;
+
+  let location = document.getElementById(id + 'morada').innerHTML;
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+    params: {
+      address: location,
+      // key: app.locals.googleMapsKey //NÃO ESTÁ A ASSUMIR
+      key: 'AIzaSyDmvhJhg_f-9Tozc1F_XkMOcaHfGCy14Nc'
+    }
+  })
+  .then((res) => {
+    console.log('geocode address param:', params.address);
+    console.log(res);
+    let formattedAddress = res.data.results[0].formatted_address;
+    let latitude = res.data.results[0].geometry.morada.lat;
+    let longitude = res.data.results[0].geometry.morada.lng;
+
+    let geometry = {
+      address: formattedAddress,
+      coords: {
+        lat: latitude,
+        lng: longitude
+      }
+    };
+
+    console.log(formattedAddress);
+
+    console.log('lat:', latitude, 'lng:', longitude);
+
+    console.log('geo:', geometry);
+
+    return geometry;
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+}
+}
